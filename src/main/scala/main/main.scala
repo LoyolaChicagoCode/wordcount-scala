8d6225bbfb2199431734904b3232c8d24413f310
Saulo Aguiar <saguiar@luc.edu>
2014-09-15 18:33:38 -0500

    Adding more tests

--- src/main/scala/main/main.scala
+++ src/main/scala/main/main.scala
@@ -10,33 +10,8 @@ object Main extends App {
     counter.account(word)
   }
 
-  for ( (k,v) <- counter.getWords()) {
+  for ((k,v) <- counter.getWords()) {
     println(k + " - " + v)
   }
 
 }
-
-/*for (word <- io.Source.stdin.getLines.flatMap(_.split("\\W+"))) {
-	println(word)
-}*/
-
-/*
-scala.io.Source.fromFile("file.txt")
-  .getLines
-  .flatMap(_.split("\\W+"))
-  .foldLeft(Map.empty[String, Int]){
-     (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-  }
-*/
-/*scala.io.Source.stdin.getLine(0) match {
-			case "hello" =>
-				println("hi")
-			case "input" =>
-				for (word <- io.Source.stdin.getLines.flatMap(_.split("\\W+"))) {
-					println(word)
-				}	
-			case "exit" =>
-				sys.exit
-			case _ =>
-				println("not found") 
-		}*/
